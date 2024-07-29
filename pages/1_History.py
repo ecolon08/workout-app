48e8d149b1987c47baa2649b9b1811ecf554fbb3
ecolon08 <ernestocolon08@gmail.com>
2024-07-28 21:39:10 -0400

    update history page and requirements.txt

--- pages/1_History.py
+++ pages/1_History.py
@@ -24,37 +24,23 @@ def get_ex_df(df, dt):
     exercises = {}
     for ex in unique_exercises:
         exercises[ex] = filter_by_exercise(df, ex)[['reps', 'weight', 'time', 'superset', 'notes']]
-    # return [filter_by_exercise(df, e) for e in unique_exercises]
+
     return exercises
 
 
 if not check_password():
     st.stop()
 
-# df_db = pd.read_csv('csv_db.csv')
-# Create a connection object
-# creds = st.secrets['gspread']['gsheets_creds']
-# gc = gspread.service_account_from_dict(creds)
-# sh = gc.open('workout_db')
-# worksheet = sh.get_worksheet(0)
 worksheet = load_gs_worksheet()
 
-# print(sh.sheet1.get('A1'))
 df_db = pd.DataFrame(worksheet.get_all_records())
 
 # date filter
 tz = pytz.timezone('America/New_York')
 dt_ny = datetime.datetime.now(tz)
-#
 d = st.date_input("Exercise Date", dt_ny.date(), format="MM/DD/YYYY").strftime("%m/%d/%y")
-# set_todays_date()
-# print(type(st.session_state['today']))
-# d = datetime.datetime.strptime(st.session_state['today'], "%m/%d/%y").date()
 st.write(d)
-# st.write(type(d))
-# d = st.date_input("Exercise Date", d, format="MM/DD/YYYY").strftime("%m/%d/%y")
 
-# exercises = get_ex_df(df_db, st.session_state['today'])
 exercises = get_ex_df(df_db, d)
 
 for e in exercises.keys():
